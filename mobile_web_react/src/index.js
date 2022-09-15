487b7c39d303bd1a037ed3d7c8980fdd01c64ee1
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2022-09-15 13:56:01 +0900

    Update index.js

--- mobile_web_react/src/index.js
+++ mobile_web_react/src/index.js
@@ -3,26 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Library from './chapter_03/ch.3 ex pure js/Library';
-// import Restaurant from './chapter_03/ch.3 ex_restaurant/Restaurant';
-import Clock from './chapter_04/Clock';
+import Restaurant from './chapter_03/ch.3 ex_restaurant/Restaurant';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     {/* 실행시 렌더링 하는 부분  */}
-//     < Clock/>
-//   </React.StrictMode>
-// );
 
-setInterval (()=>{
-  ReactDOM.render(
-    <React.StrictMode>
-      <Clock/>
-    </React.StrictMode>,
-    document.getElementById('root')
-    );
-},1000);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* 실행시 렌더링 하는 부분  */}
+    < Restaurant/>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
